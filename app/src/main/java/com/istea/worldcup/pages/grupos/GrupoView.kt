bd710b6adf172977c0a142c4f89c95799c8a6aa0
Nicolás Enciso <enectrl@gmail.com>
2025-12-09 19:48:24 -0300

    fix: Display group name instead of literal string in GrupoView
    - Change the `Text` composable to use the `grupo.name` property instead of the hardcoded string `"grupo.name"`.

--- app/src/main/java/com/istea/worldcup/pages/grupos/GrupoView.kt
+++ app/src/main/java/com/istea/worldcup/pages/grupos/GrupoView.kt
@@ -82,7 +82,7 @@ fun GroupCardView(
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Text(
-                text = "grupo.name",
+                text = grupo.name,
                 style = MaterialTheme.typography.titleLarge,
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center
