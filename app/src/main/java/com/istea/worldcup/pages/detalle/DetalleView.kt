d84390b85993bbbc7901c568d282630bf6c82d82
Nicolás Enciso <enectrl@gmail.com>
2025-12-09 19:58:39 -0300

    feat: Use theme color for TopAppBar in DetalleView
    - Update the `containerColor` of the `TopAppBar` to use `MaterialTheme.colorScheme.primaryContainer` instead of a hardcoded `Color.Green`.

--- app/src/main/java/com/istea/worldcup/pages/detalle/DetalleView.kt
+++ app/src/main/java/com/istea/worldcup/pages/detalle/DetalleView.kt
@@ -61,7 +61,7 @@ fun DetalleView (
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.Green,
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
                 ),
                 title = { Text(text = "Partidos") },
